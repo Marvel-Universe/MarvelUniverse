2944e9c5524ae9a9f9146785eb0d03b8917896b1
Jiratchaya Thongsuthum <92805190+mjrchy@users.noreply.github.com>
2023-10-26 23:48:44 +0700

    Setting placeholder and password

--- mysite/forms.py
+++ mysite/forms.py
@@ -4,9 +4,12 @@ from django.contrib.auth.models import User  # Import user model
 
 
 class SignupForm(UserCreationForm):
-    email = forms.EmailField(required=True)
-    firstname = forms.CharField(max_length=30, required=True)
-    lastname = forms.CharField(max_length=30, required=True)
+    email = forms.EmailField(required=True, widget=forms.TextInput(attrs={'placeholder': 'Email'}))
+    firstname = forms.CharField(max_length=30, required=True, widget=forms.TextInput(attrs={'placeholder': 'Firstname'}))
+    lastname = forms.CharField(max_length=30, required=True, widget=forms.TextInput(attrs={'placeholder': 'Lastname'}))
+    username =  forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Username'}))
+    password1 = forms.CharField(label="Password", widget=forms.PasswordInput(attrs={'placeholder':'password'}),)
+    password2 = forms.CharField(label="Confirm password", widget=forms.PasswordInput(attrs={'placeholder':'Confirm password'}),)
 
     class Meta:
         model = User
