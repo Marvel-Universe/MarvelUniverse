8c86531485027e206b16baad3c27388056bdb82b
Sankrub <navathon.l@ku.th>
2023-10-27 20:12:58 +0700

    add path for google mail login

--- mysite/urls.py
+++ mysite/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 from django.views.generic import RedirectView
 from django.contrib.auth import views as auth_views
 from . import views
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -27,4 +28,8 @@ urlpatterns = [
     path('accounts/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('signup/', views.signup, name='signup'),
+    path('', TemplateView.as_view(template_name="login_page/index.html")),
+    path('accounts/', include('allauth.urls')),
+
+
 ]
