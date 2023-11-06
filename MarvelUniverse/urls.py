7a6ebd0fd4e1575a770ff80b9a45ba82210bfbcd
Jiratchaya Thongsuthum <92805190+mjrchy@users.noreply.github.com>
2023-11-06 10:20:34 +0700

    Add app_name

--- MarvelUniverse/urls.py
+++ MarvelUniverse/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from .views.homepage import HomePageView
+from .views.see_all import SeeAllCharactersView
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
+    path('all-characters/', SeeAllCharactersView.as_view(), name="all-characters"),
+    path('all-comics/', SeeAllCharactersView.as_view(), name="all-characters"),
+    path('all-series', SeeAllCharactersView.as_view(), name="all-characters"),    
 ]
