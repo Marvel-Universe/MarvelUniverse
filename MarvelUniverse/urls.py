9c3216977d9be6cf2cc31e17ff79fa4e72249ece
Jiratchaya Thongsuthum <92805190+mjrchy@users.noreply.github.com>
2023-11-06 13:46:34 +0700

    Add all-characters, all-comics and all-series urls

--- MarvelUniverse/urls.py
+++ MarvelUniverse/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from .views.homepage import HomePageView
-from .views.see_all import SeeAllCharactersView
+from .views.see_all import SeeAllCharactersView, SeeAllComicsView, SeeAllSeriesView
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
     path('all-characters/', SeeAllCharactersView.as_view(), name="all-characters"),
-    path('all-comics/', SeeAllCharactersView.as_view(), name="all-characters"),
-    path('all-series', SeeAllCharactersView.as_view(), name="all-characters"),    
+    path('all-comics/', SeeAllComicsView.as_view(), name="all-comics"),
+    path('all-series/', SeeAllSeriesView.as_view(), name="all-series"),    
 ]
