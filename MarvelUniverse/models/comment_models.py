d8abfde1081215c064d06382793b328036f559cf
Jiratchaya Thongsuthum <92805190+mjrchy@users.noreply.github.com>
2023-11-12 16:11:00 +0700

    Rename the attribute in SeriesComment model

--- MarvelUniverse/models/comment_models.py
+++ MarvelUniverse/models/comment_models.py
@@ -1,12 +1,12 @@
 from django.db import models
 from .marvel_models import Series
+from django.contrib.auth.models import User
 
 
-class SeriesReview(models.Model):
+class SeriesComment(models.Model):
     series = models.ForeignKey(Series, on_delete=models.CASCADE, related_name='comments')
-    name = models.CharField(max_length=30)
-    email = models.EmailField()
-    body = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comments')
+    user_comment = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     active = models.BooleanField(default=False)
 
@@ -14,5 +14,5 @@ class SeriesReview(models.Model):
         ordering = ['created_on']
 
     def __str__(self):
-        return 'Comment {} by {}'.format(self.body, self.name)
-
+        return 'Comment {} by {}'.format(self.user_comment, self.user)
+    
