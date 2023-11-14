33cf322cd521c0df432acefe00348d353f3633c9
Sankrub <navathon.l@ku.th>
2023-11-14 21:44:54 +0700

    update search in see all for every categories

--- MarvelUniverse/views/see_all.py
+++ MarvelUniverse/views/see_all.py
@@ -8,17 +8,17 @@ class AllCharactersView(View):
     template_name = 'MarvelUniverse/see_all/all_characters.html'
 
     def get(self, request):
-        if "get_search" in request.GET:
-            get_search = request.GET["get_search"]
-            characters = Character.objects.filter(name__icontains=get_search)[:]
+        search_value = request.GET.get("get_search", "")
 
+        if search_value:
+            characters = Character.objects.filter(name__icontains=search_value)
         else:
             characters = Character.objects.all()
 
         context = {
             'characters': characters,
             'character_count': characters.count(),
-            'get_search': request.GET["get_search"]
+            'get_search': search_value,
         }
         return render(request, self.template_name, context)
 
@@ -27,32 +27,33 @@ class AllComicsView(View):
     template_name = 'MarvelUniverse/see_all/all_comics.html'
 
     def get(self, request):
-        if "get_search" in request.GET:
-            get_search = request.GET["get_search"]
-            comics = Comic.objects.filter(title__icontains=get_search)[:]
+        search_value = request.GET.get("get_search", "")
+        if search_value:
+            comics = Comic.objects.filter(title__icontains=search_value)[:]
         else:
             comics = Comic.objects.all()
 
         context = {
             'comics': comics,
-            'comics_count': comics.count()
+            'comics_count': comics.count(),
+            'get_search': search_value,
         }
         return render(request, self.template_name, context)
-    
+
 
 class AllSeriesView(View):
     template_name = 'MarvelUniverse/see_all/all_series.html'
 
     def get(self, request):
-        if "get_search" in request.GET:
-            get_search = request.GET["get_search"]
-            series = Series.objects.filter(title__icontains=get_search)[:]
+        search_value = request.GET.get("get_search", "")
+        if search_value:
+            series = Series.objects.filter(title__icontains=search_value)[:]
         else:
             series = Series.objects.all()
 
         context = {
             'series': series,
-            'series_count': series.count()
+            'series_count': series.count(),
+            'get_search': search_value,
         }
         return render(request, self.template_name, context)
-    
