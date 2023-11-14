09e05714244dd50a709b0faac02d63fe189878d2
Jiratchaya Thongsuthum <92805190+mjrchy@users.noreply.github.com>
2023-11-14 11:08:10 +0700

    Delete unrelated urls

--- MarvelUniverse/urls.py
+++ MarvelUniverse/urls.py
@@ -1,15 +1,12 @@
 from django.urls import path
 from .views.homepage import HomePageView
 from .views.see_all import AllCharactersView, AllComicsView, AllSeriesView
-from .views.detail import CharactersDetailView, comic_detail_view, series_detail_view
+
 
 app_name = "MarvelUniverse"
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
-    path('all-characters/', AllCharactersView.as_view(), name="all-characters"),
-    path('all-comics/', AllComicsView.as_view(), name="all-comics"),
-    path('all-series/', AllSeriesView.as_view(), name="all-series"),   
-    path('characters/<int:character_pk>', CharactersDetailView.as_view(), name="characters-detail"),        
-    path('comics/<int:comic_pk>', comic_detail_view, name="comics-detail"),        
-    path('series/<int:series_pk>', series_detail_view, name="series-detail"),        
+    path('characters/', AllCharactersView.as_view(), name="all-characters"),
+    path('comics/', AllComicsView.as_view(), name="all-comics"),
+    path('series/', AllSeriesView.as_view(), name="all-series"),          
 ]
