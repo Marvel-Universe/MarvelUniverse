c3c6c57019d8bfd94dcb07ee8aaebf1b052d50e5
Sankrub <navathon.l@ku.th>
2023-11-16 02:55:47 +0700

    add reset password

--- mysite/urls.py
+++ mysite/urls.py
@@ -20,7 +20,6 @@ from django.contrib.auth import views as auth_views
 from . import views
 from django.views.generic import TemplateView
 
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', RedirectView.as_view(url='marvel-universe/')),
@@ -31,4 +30,16 @@ urlpatterns = [
     path('signup/', views.signup, name='signup'),
     path('', TemplateView.as_view(template_name="login_page/index.html")),
     path('accounts/', include('allauth.urls')),
+    path('reset_password/',
+         auth_views.PasswordResetView.as_view(template_name="registration/password_reset.html"),
+         name='reset_password'),
+    path('reset_password_sent/',
+         auth_views.PasswordResetDoneView.as_view(template_name="registration/password_reset_sent.html"),
+         name='password_reset_done'),
+    path('reset/<uidb64>/<token>/',
+         auth_views.PasswordResetConfirmView.as_view(template_name="registration/password_reset_form.html"),
+         name='password_reset_confirm'),
+    path('reset_password_complete/',
+         auth_views.PasswordResetCompleteView.as_view(template_name="registration/password_reset_complete.html"),
+         name='password_reset_complete'),
 ]
