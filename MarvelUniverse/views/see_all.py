e209e6d0d136f97c9be5516cb5f8992622bb2383
Jiratchaya Thongsuthum <92805190+mjrchy@users.noreply.github.com>
2023-11-16 00:49:42 +0700

    Add value for search box

--- MarvelUniverse/views/see_all.py
+++ MarvelUniverse/views/see_all.py
@@ -8,16 +8,17 @@ class AllCharactersView(View):
     template_name = 'MarvelUniverse/see_all/all_characters.html'
 
     def get(self, request):
-        if "get_search" in request.GET:
+        search_value = request.GET.get("get_search", "")
+        if search_value:
             get_search = request.GET["get_search"]
             characters = Character.objects.filter(name__icontains=get_search)[:]
-
         else:
             characters = Character.objects.all()
 
         context = {
             'characters': characters,
-            'character_count': characters.count()
+            'character_count': characters.count(),
+            'get_search': search_value
         }
         return render(request, self.template_name, context)
 
@@ -26,7 +27,8 @@ class AllComicsView(View):
     template_name = 'MarvelUniverse/see_all/all_comics.html'
 
     def get(self, request):
-        if "get_search" in request.GET:
+        search_value = request.GET.get("get_search", "")
+        if search_value:
             get_search = request.GET["get_search"]
             comics = Comic.objects.filter(title__icontains=get_search)[:]
         else:
@@ -34,7 +36,8 @@ class AllComicsView(View):
 
         context = {
             'comics': comics,
-            'comics_count': comics.count()
+            'comics_count': comics.count(),
+            'get_search': search_value
         }
         return render(request, self.template_name, context)
     
@@ -43,7 +46,8 @@ class AllSeriesView(View):
     template_name = 'MarvelUniverse/see_all/all_series.html'
 
     def get(self, request):
-        if "get_search" in request.GET:
+        search_value = request.GET.get("get_search", "")
+        if search_value:
             get_search = request.GET["get_search"]
             series = Series.objects.filter(title__icontains=get_search)[:]
         else:
@@ -51,7 +55,8 @@ class AllSeriesView(View):
 
         context = {
             'series': series,
-            'series_count': series.count()
+            'series_count': series.count(),
+            'get_search': search_value
         }
         return render(request, self.template_name, context)
     
