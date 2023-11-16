c67d34b4d6782393e224d439bfb87ef302e34d94
Jiratchaya Thongsuthum <92805190+mjrchy@users.noreply.github.com>
2023-11-16 23:02:12 +0700

    Add detail page urls

--- MarvelUniverse/urls.py
+++ MarvelUniverse/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .views.homepage import HomePageView
 from .views.see_all import AllCharactersView, AllComicsView, AllSeriesView
+from .views.detail import CharactersDetailView, comic_detail_view, series_detail_view
 
 
 app_name = "MarvelUniverse"
@@ -8,5 +9,8 @@ urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
     path('characters/', AllCharactersView.as_view(), name="characters"),
     path('comics/', AllComicsView.as_view(), name="comics"),
-    path('series/', AllSeriesView.as_view(), name="series"),          
+    path('series/', AllSeriesView.as_view(), name="series"),   
+    path('characters/<int:character_pk>', CharactersDetailView.as_view(), name="characters-detail"),        
+    path('comics/<int:comic_pk>', comic_detail_view, name="comics-detail"),        
+    path('series/<int:series_pk>', series_detail_view, name="series-detail")       
 ]
