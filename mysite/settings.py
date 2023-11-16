26e7f1ac073d90cf884c23eb12de46788248d58a
Sankrub <navathon.l@ku.th>
2023-11-16 12:33:40 +0700

    update mysite

--- mysite/settings.py
+++ mysite/settings.py
@@ -163,5 +163,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587  # Use port 465 for SSL
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'navalima549@gmail.com'
-EMAIL_HOST_PASSWORD = 'qpoh vvyr yoso lkrq'
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
