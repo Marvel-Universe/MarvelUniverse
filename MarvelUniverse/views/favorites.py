ebecac6808e7b78e6ad7a70e04c310e721956cb8
Warat Poovorakit <warat.po@ku.th>
2023-11-17 16:09:14 +0700

    implement favorites button

---
+++ MarvelUniverse/views/favorites.py
@@ -0,0 +1,56 @@
+from django.contrib.auth.decorators import login_required
+from django.http import JsonResponse
+from django.views import View
+from MarvelUniverse.models import Character, Comic, Series, FavoriteCharacter, FavoriteComic, FavoriteSeries
+from django.shortcuts import render
+
+
+class FavoriteView(View):
+    template_name = 'MarvelUniverse/favorites.html'
+
+    def get(self, request):
+        favorite_character = FavoriteCharacter.objects.filter(user=request.user)
+        favorite_comic = FavoriteComic.objects.filter(user=request.user)
+        favorite_series = FavoriteSeries.objects.filter(user=request.user)
+        characters_list = [favorite.character for favorite in favorite_character]
+        comics_list = [favorite.comic for favorite in favorite_comic]
+        series_list = [favorite.series for favorite in favorite_series]
+        context = {
+            'characters_list': characters_list,
+            'comics_list': comics_list,
+            'series_list': series_list,
+            'characters_count': len(characters_list),
+            'comics_count': len(comics_list),
+            'series_count': len(series_list)
+        }
+        return render(request, self.template_name, context)
+
+
+@login_required(login_url='login')
+def toggle_favorite(request, model, object_id):
+    user = request.user
+    is_favorite = False
+
+    model_class_map = {
+        'character': (Character, FavoriteCharacter),
+        'comic': (Comic, FavoriteComic),
+        'series': (Series, FavoriteSeries),
+    }
+
+    model_class = model_class_map.get(model)
+    if model_class:
+        obj = model_class[0].objects.filter(pk=object_id).first()
+        favorite_model = model_class[1]
+
+        if obj:
+            favorite_instance, created = favorite_model.objects.get_or_create(user=user, **{model: obj})
+            if not created:
+                favorite_instance.delete()
+            else:
+                is_favorite = True
+        else:
+            return JsonResponse({'error': 'Object not found'})
+    else:
+        return JsonResponse({'error': 'Invalid model type'})
+
+    return JsonResponse({'is_favorite': is_favorite})
