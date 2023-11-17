b32e3e665bae9e196cf90a417811590465ed36e0
Warat Poovorakit <warat.po@ku.th>
2023-11-17 16:13:08 +0700

    implement favorites model

--- MarvelUniverse/models/__init__.py
+++ MarvelUniverse/models/__init__.py
@@ -1,2 +1,3 @@
 from .marvel_models import Character, Comic, Series, CharacterInComic, CharacterInSeries
-from .comment_models import SeriesComment
+from .comment_models import SeriesComment, ComicComment
+from .favorites import FavoriteCharacter, FavoriteComic, FavoriteSeries
