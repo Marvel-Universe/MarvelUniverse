21fe0750e5d1f89e9284693d271e33d87e80b817
Warat Poovorakit <warat.po@ku.th>
2023-11-17 16:15:30 +0700

    update details for each context

--- MarvelUniverse/views/detail.py
+++ MarvelUniverse/views/detail.py
@@ -1,11 +1,14 @@
-from django.urls import reverse
 from django.shortcuts import render
 from django.views import View
+from ..models import Character, Comic, Series, CharacterInComic, CharacterInSeries
 from django.contrib import messages
-from django.http import HttpResponseRedirect
+from django.shortcuts import render
 from ..forms import SeriesCommentForm, ComicCommentForm
 from ..models.comment_models import SeriesComment, ComicComment
-from ..models import Character, Comic, Series, CharacterInComic, CharacterInSeries
+from ..models import FavoriteCharacter, FavoriteComic, FavoriteSeries
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 
 class CharactersDetailView(View):
@@ -16,28 +19,34 @@ class CharactersDetailView(View):
             character = Character.objects.get(pk=character_pk)
         except Character.DoesNotExist:
             messages.error(f"Character {character_pk} not found")
-            return HttpResponseRedirect(reverse('MarvelUniverse:characters'))
+            return HttpResponseRedirect(reverse('MarvelUniverse:all-characters'))
         else:
             characters_comics = CharacterInComic.objects.filter(character=character)
             characters_series = CharacterInSeries.objects.filter(character=character)
         comics_list = [character_comic.comic for character_comic in characters_comics]
         series_list = [character_series.series for character_series in characters_series]
+        if request.user.is_authenticated:
+            is_favorite = FavoriteCharacter.objects.filter(user=request.user, character=character).exists()
+        else:
+            is_favorite = False
         context = {
             'character': character,
             'comics_list': comics_list,
             'series_list': series_list,
             'comics_count': characters_comics.count(),
             'series_count': characters_series.count(),
+            'is_favorite': is_favorite
         }
-        return render(request, self.template_name, context)   
+        return render(request, self.template_name, context)
 
 
+# @login_required(login_url='login')
 def comic_detail_view(request, comic_pk):
     try:
         comic = Comic.objects.get(pk=comic_pk)
     except Comic.DoesNotExist:
         messages.error(f"Comic {comic_pk} not found")
-        return HttpResponseRedirect(reverse('MarvelUniverse:comics'))
+        return HttpResponseRedirect(reverse('MarvelUniverse:all-comics'))
     else:
         characters_series = CharacterInComic.objects.filter(comic=comic)
         characters_list = [character_series.character for character_series in characters_series]
@@ -55,6 +64,10 @@ def comic_detail_view(request, comic_pk):
             initial_data = {'user_comment': ''}
             comment_form = ComicCommentForm(initial=initial_data)
     comic_comments = ComicComment.objects.filter(comic=comic, active=True)
+    if request.user.is_authenticated:
+        is_favorite = FavoriteComic.objects.filter(user=request.user, comic=comic).exists()
+    else:
+        is_favorite = False
     context = {
         'comic': comic,
         'characters_list': characters_list,
@@ -62,16 +75,18 @@ def comic_detail_view(request, comic_pk):
         'comic_comments': comic_comments,
         'comic_comments_count': comic_comments.count(),
         'comment_form': comment_form,
+        'is_favorite': is_favorite
     }
     return render(request, 'MarvelUniverse/detail/comics.html', context)
 
 
+# @login_required(login_url='login')
 def series_detail_view(request, series_pk):
     try:
         series = Series.objects.get(pk=series_pk)
     except Series.DoesNotExist:
         messages.error(f"Series {series_pk} not found")
-        return HttpResponseRedirect(reverse('MarvelUniverse:series'))
+        return HttpResponseRedirect(reverse('MarvelUniverse:all-series'))
     else:
         characters_series = CharacterInSeries.objects.filter(series=series)
         characters_list = [character_series.character for character_series in characters_series]
@@ -90,12 +105,17 @@ def series_detail_view(request, series_pk):
             initial_data = {'user_comment': ''}
             comment_form = SeriesCommentForm(initial=initial_data)
     series_comments = SeriesComment.objects.filter(series=series, active=True)
+    if request.user.is_authenticated:
+        is_favorite = FavoriteSeries.objects.filter(user=request.user, series=series).exists()
+    else:
+        is_favorite = False
     context = {
         'series': series,
         'characters_list': characters_list,
         'characters_count': characters_series.count(),
         'series_comments': series_comments,
         'series_comments_count': series_comments.count(),
-        'comment_form': comment_form
+        'comment_form': comment_form,
+        'is_favorite': is_favorite
     }
     return render(request, 'MarvelUniverse/detail/series.html', context)
