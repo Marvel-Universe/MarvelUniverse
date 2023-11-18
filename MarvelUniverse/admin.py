283962fbd6b3d57b3c4ce21a82cf365933a01bce
Sankrub <navathon.l@ku.th>
2023-11-18 15:04:58 +0700

    add character comment in admin

--- MarvelUniverse/admin.py
+++ MarvelUniverse/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from .models.marvel_models import Character, Comic, Series, CharacterInComic, CharacterInSeries
-from .models.comment_models import SeriesComment, ComicComment
+from .models.comment_models import SeriesComment, ComicComment, CharacterComment
 
 # Register your models here.
 admin.site.register(Character)
@@ -14,18 +14,28 @@ admin.site.register(CharacterInSeries)
 class SeriesCommentAdmin(admin.ModelAdmin):
     list_display = ('user', 'user_comment', 'series', 'created_on', 'active')
     list_filter = ('active', 'created_on')
-    search_fields = ('user', 'email', 'user_comment')
+    search_fields = ('user', 'email', 'user_comment') # Note the use of user__username to search by username
     actions = ['approve_comments']
 
     def approve_comments(self, request, queryset):
         queryset.update(active=True)
 
 
+@admin.register(CharacterComment)
+class CharacterCommentAdmin(admin.ModelAdmin):
+    list_display = ('user', 'user_comment', 'character', 'created_on', 'active')
+    list_filter = ('active', 'created_on')
+    search_fields = ('user__username', 'email', 'user_comment')
+    actions = ['approve_comments']
+
+    def approve_comments(self, request, queryset):
+        queryset.update(active=True)
+
 @admin.register(ComicComment)
 class ComicCommentAdmin(admin.ModelAdmin):
     list_display = ('user', 'user_comment', 'comic', 'created_on', 'active')
     list_filter = ('active', 'created_on')
-    search_fields = ('user__username', 'email', 'user_comment')  # Note the use of user__username to search by username
+    search_fields = ('user__username', 'email', 'user_comment')
     actions = ['approve_comments']
 
     def approve_comments(self, request, queryset):
