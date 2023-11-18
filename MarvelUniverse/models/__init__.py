6fcc89b8d95a7f14235a9d6a8384447d76eb3c5f
Sankrub <navathon.l@ku.th>
2023-11-18 15:05:34 +0700

    update after add character comment

--- MarvelUniverse/models/__init__.py
+++ MarvelUniverse/models/__init__.py
@@ -1,3 +1,3 @@
 from .marvel_models import Character, Comic, Series, CharacterInComic, CharacterInSeries
-from .comment_models import SeriesComment, ComicComment
+from .comment_models import SeriesComment, ComicComment, CharacterComment
 from .favorites import FavoriteCharacter, FavoriteComic, FavoriteSeries
