7d94649a3e0ce1cf8b77c3be57bc38cc85bc8289
Jiratchaya Thongsuthum <92805190+mjrchy@users.noreply.github.com>
2023-11-19 16:49:04 +0700

    Implement quiz models

--- MarvelUniverse/models/__init__.py
+++ MarvelUniverse/models/__init__.py
@@ -1,3 +1,4 @@
 from .marvel_models import Character, Comic, Series, CharacterInComic, CharacterInSeries
 from .comment_models import SeriesComment, ComicComment, CharacterComment
 from .favorites import FavoriteCharacter, FavoriteComic, FavoriteSeries
+from .quiz import CharacterQuestion, CharacterChoice, SeriesQuestion, SeriesChoice, ComicQuestion, ComicChoice
