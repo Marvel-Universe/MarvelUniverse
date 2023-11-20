0204d9ca27699fd236402cdaccb42b20b5a131e3
Sankrub <navathon.l@ku.th>
2023-11-20 15:56:45 +0700

    update fav

--- MarvelUniverse/views/favorites.py
+++ MarvelUniverse/views/favorites.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.decorators import login_required
 from django.http import JsonResponse
 from django.views import View
-from MarvelUniverse.models import Character, Comic, Series, FavoriteCharacter, FavoriteComic, FavoriteSeries
+from ..models import Character, Comic, Series, FavoriteCharacter, FavoriteComic, FavoriteSeries
 from django.shortcuts import render
 
 
