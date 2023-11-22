5a4cd47bce4d85db9c7a7bd3919e994251ca82a0
Warat Poovorakit <warat.po@ku.th>
2023-11-23 04:14:40 +0700

    apply docstring for forms on website

--- MarvelUniverse/forms.py
+++ MarvelUniverse/forms.py
@@ -3,16 +3,39 @@ from django import forms
 
 
 class SeriesCommentForm(forms.ModelForm):
+    """
+    Form for adding a comment to a Series.
+
+    Fields:
+    - user_comment (str): The content of the comment.
+
+    """
     class Meta:
         model = SeriesComment
         fields = ['user_comment']
 
+
 class ComicCommentForm(forms.ModelForm):
+    """
+    Form for adding a comment to a Comic.
+
+    Fields:
+    - user_comment (str): The content of the comment.
+
+    """
     class Meta:
         model = ComicComment
         fields = ['user_comment']
 
+
 class CharacterCommentForm(forms.ModelForm):
+    """
+    Form for adding a comment to a Character.
+
+    Fields:
+    - user_comment (str): The content of the comment.
+
+    """
     class Meta:
         model = CharacterComment
         fields = ['user_comment']
