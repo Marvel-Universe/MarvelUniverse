10287270ee2a1b1d1eb7a5ab9f4d9dc124dc0302
Jiratchaya Thongsuthum <92805190+mjrchy@users.noreply.github.com>
2023-11-22 13:47:58 +0700

    Rename quiz model file

--- MarvelUniverse/models/__init__.py
+++ MarvelUniverse/models/__init__.py
@@ -1,4 +1,4 @@
 from .marvel_models import Character, Comic, Series, CharacterInComic, CharacterInSeries
 from .comment_models import SeriesComment, ComicComment, CharacterComment
 from .favorites import FavoriteCharacter, FavoriteComic, FavoriteSeries
-from .quiz import CharacterQuestion, CharacterChoice, SeriesQuestion, SeriesChoice, ComicQuestion, ComicChoice
+from .quiz_models import CharacterQuestion, CharacterChoice, SeriesQuestion, SeriesChoice, ComicQuestion, ComicChoice
