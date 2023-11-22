68b70bc04a582ad65dd56227c2a3f18eaddc6b6f
Jiratchaya Thongsuthum <92805190+mjrchy@users.noreply.github.com>
2023-11-22 13:57:51 +0700

    Rename quiz url

--- MarvelUniverse/urls.py
+++ MarvelUniverse/urls.py
@@ -5,7 +5,7 @@ from .views.see_all import AllCharactersView, AllComicsView, AllSeriesView
 from .views.detail import character_detail_view, comic_detail_view, series_detail_view
 from .views.favorites import toggle_favorite
 from .views.favorites import FavoriteView
-from .views.quiz import StartQuizView, CharacterInstructionView, ComicInstructionView, SeriesInstructionView, random_quiz
+from .views.quiz import SelectQuizView, CharacterInstructionView, ComicInstructionView, SeriesInstructionView, random_quiz
 
 
 app_name = "MarvelUniverse"
@@ -21,7 +21,7 @@ urlpatterns = [
     path('series/<int:series_pk>', series_detail_view, name="series-detail"),
     path('about/', AboutUsView.as_view(), name='about'),
     # quiz url
-    path('quiz/', StartQuizView.as_view(), name='start-quiz'),
+    path('quiz/', SelectQuizView.as_view(), name='select-quiz'),
     # quiz instruction
     path('quiz/character-instruction/', CharacterInstructionView.as_view(), name='character-instruction'),
     path('quiz/comic-instruction/', ComicInstructionView.as_view(), name='comic-instruction'),
