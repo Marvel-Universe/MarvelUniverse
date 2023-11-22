76264052d5184b53859a131ebf80f2dfd188489c
Warat Poovorakit <warat.po@ku.th>
2023-11-23 04:14:16 +0700

    put docstring for application

--- MarvelUniverse/apps.py
+++ MarvelUniverse/apps.py
@@ -2,5 +2,13 @@ from django.apps import AppConfig
 
 
 class MarveluniverseConfig(AppConfig):
+    """
+    AppConfig for the MarvelUniverse app.
+
+    Attributes:
+    - default_auto_field (str): The default value for the 'AutoField' option in model definitions.
+    - name (str): The name of the app.
+
+    """
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'MarvelUniverse'
