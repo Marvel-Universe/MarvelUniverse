88ce157a9d01671220c764e01504635013a9ca60
Jiratchaya Thongsuthum <92805190+mjrchy@users.noreply.github.com>
2023-11-23 14:03:58 +0700

    Add quiz urls

--- MarvelUniverse/urls.py
+++ MarvelUniverse/urls.py
@@ -5,7 +5,7 @@ from .views.see_all import AllCharactersView, AllComicsView, AllSeriesView
 from .views.detail import character_detail_view, comic_detail_view, series_detail_view
 from .views.favorites import toggle_favorite
 from .views.favorites import FavoriteView
-from .views.quiz import SelectQuizView, CharacterInstructionView, ComicInstructionView, SeriesInstructionView, random_quiz
+from .views.quiz import SelectQuizView, CharacterInstructionView, ComicInstructionView, SeriesInstructionView, random_quiz, character_quiz_view, comic_quiz_view, series_quiz_view, quiz_results
 
 
 app_name = "MarvelUniverse"
@@ -23,8 +23,18 @@ urlpatterns = [
     # quiz url
     path('quiz/', SelectQuizView.as_view(), name='select-quiz'),
     # quiz instruction
+    path('quiz/random-quiz/', random_quiz, name='random-quiz'),
     path('quiz/character-instruction/', CharacterInstructionView.as_view(), name='character-instruction'),
     path('quiz/comic-instruction/', ComicInstructionView.as_view(), name='comic-instruction'),
     path('quiz/series-instruction/', SeriesInstructionView.as_view(), name='series-instruction'),
-    path('quiz/random-quiz/', random_quiz, name='random-quiz'),
+    
+    # path('quiz/character-quiz/', random_character_question, name='random-character-question'),
+    # path('quiz/comic-quiz/', random_comic_question, name='random-comic-question'),
+    # path('quiz/series-quiz/', random_series_question, name='random-series-question'),
+
+    path('quiz/character-quiz/<int:question_pk>', character_quiz_view, name='character-quiz'),
+    path('quiz/comic-quiz/<int:question_pk>', comic_quiz_view, name='comic-quiz'),
+    path('quiz/series-quiz/<int:question_pk>', series_quiz_view, name='series-quiz'),
+
+    path('quiz/results', quiz_results, name='quiz-results'),
 ]
