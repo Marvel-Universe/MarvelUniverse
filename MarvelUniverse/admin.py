eb584fd1b9c5bfae7a98930bfceea9b2ea19a41b
Sankrub <navathon.l@ku.th>
2023-11-23 22:04:32 +0700

    add the userdata in admin

--- MarvelUniverse/admin.py
+++ MarvelUniverse/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models.marvel_models import Character, Comic, Series, CharacterInComic, CharacterInSeries
 from .models.comment_models import SeriesComment, ComicComment, CharacterComment
+from .models.user_data_models import UserData
 
 # Register your models here.
 admin.site.register(Character)
@@ -40,3 +41,8 @@ class ComicCommentAdmin(admin.ModelAdmin):
 
     def approve_comments(self, request, queryset):
         queryset.update(active=True)
+
+@admin.register(UserData)
+class UserDataAdmin(admin.ModelAdmin):
+    list_display = ('user', 'profile_img_url', 'trophy_img', 'scores')
+    search_fields = ('user__username',)
