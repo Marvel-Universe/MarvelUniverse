932ebec0ed62817d1509781d88eacbb758ed286c
Sankrub <navathon.l@ku.th>
2023-11-23 13:55:08 +0700

    add rank img in userdata models

--- MarvelUniverse/models/user_data_models.py
+++ MarvelUniverse/models/user_data_models.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import User
 class UserData(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='user_data')
     profile_img_url = models.URLField(max_length=500, blank=True, null=True)  # get link from character
+    rank_img_url = models.URLField(max_length=500, blank=True, null=True)  # get rank img base on score
     scores = models.IntegerField(default=0)
 
     def __str__(self):
