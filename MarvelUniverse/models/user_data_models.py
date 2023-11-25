0dc0f14bf19c46a737405960b03832735fcc4a51
Warat Poovorakit <warat.po@ku.th>
2023-11-26 04:18:12 +0700

    update user model for user page

--- MarvelUniverse/models/user_data_models.py
+++ MarvelUniverse/models/user_data_models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 class UserData(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='user_data')
     profile_img_url = models.URLField(max_length=500, blank=True, null=True, default="")
-    trophy_img = models.URLField(max_length=100, blank=True, null=True, default="")
+    trophy_img = models.URLField(max_length=100, blank=True, null=True, default="https://i.ibb.co/C8D4SXz/bronze-medal.png")
     scores = models.IntegerField(default=0)
 
     def __str__(self):
