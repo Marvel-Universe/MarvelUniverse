806c24cfb69544ce2f3578c1935d14806d7e74e1
Sankrub <navathon.l@ku.th>
2023-11-26 17:33:43 +0700

    add test for favorites view

--- MarvelUniverse/tests/test_favorite_view.py
+++ MarvelUniverse/tests/test_favorite_view.py
@@ -1,13 +1,10 @@
 import os
-from django.test import TestCase, RequestFactory
+
 from django.contrib.auth.models import User
 from django.urls import reverse
-from ..views.favorites import FavoriteView
 from ..models import Character, Comic, Series, FavoriteCharacter, FavoriteComic, FavoriteSeries
 from django.core.management import call_command
-from django.urls import clear_url_caches
-
-from django.test import Client
+from django.test import TestCase, Client
 
 
 class FavoriteViewTest(TestCase):
@@ -15,7 +12,7 @@ class FavoriteViewTest(TestCase):
         call_command('loaddata', os.getcwd() + '/data/google-oauth-data.json', '--exclude=contenttypes')
         self.client = Client()
         self.favorite_path = reverse('MarvelUniverse:favorites')
-        self.user = User.objects.create_user(username='testuser', password='testpass')
+        self.user = User.objects.create_user(username='San teset', password='test1234')
         self.character = Character.objects.create(name='Spider-Man')
         self.comic = Comic.objects.create(title='Amazing Spider-Man #1')
         self.series = Series.objects.create(title='The Spider-Man Chronicles')
@@ -34,4 +31,28 @@ class FavoriteViewTest(TestCase):
         self.assertIn(self.character, response.context['characters_list'])
         self.assertIn(self.comic, response.context['comics_list'])
         self.assertIn(self.series, response.context['series_list'])
+
+
+class ToggleFavoriteTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.user = User.objects.create_user(username='Oattest', password='oat123')
+        self.character = Character.objects.create(name='Spider-Man')
+        self.comic = Comic.objects.create(title='Amazing Spider-Man #1')
+        self.series = Series.objects.create(title='The Spider-Man Chronicles')
+
+        self.client.force_login(self.user)
+
+    def test_toggle_favorite_character(self):
+        toggle_url = reverse('MarvelUniverse:toggle_favorite', args=['character', self.character.id])
+
+        response = self.client.post(toggle_url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue(response.json()['is_favorite'])
+        self.assertTrue(FavoriteCharacter.objects.filter(user=self.user, character=self.character).exists())
+
+        response = self.client.post(toggle_url)
+        self.assertEqual(response.status_code, 200)
+        self.assertFalse(response.json()['is_favorite'])
+        self.assertFalse(FavoriteCharacter.objects.filter(user=self.user, character=self.character).exists())
 
