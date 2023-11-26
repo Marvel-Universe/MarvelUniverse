9945535292e67aa270c83693694bf9c2aa0dd96c
Sankrub <navathon.l@ku.th>
2023-11-26 20:22:02 +0700

    add docstring

--- MarvelUniverse/tests/test_favorite_view.py
+++ MarvelUniverse/tests/test_favorite_view.py
@@ -8,7 +8,14 @@ from django.test import TestCase, Client
 
 
 class FavoriteViewTest(TestCase):
+    """
+    Test suite for the FavoriteView, which shows the user's favorite characters, comics, and series.
+    """
     def setUp(self):
+        """
+        Set up for testing the FavoriteView.
+        Loads initial data, creates a test user, and favorite instances for character, comic, and series.
+        """
         call_command('loaddata', os.getcwd() + '/data/google-oauth-data.json', '--exclude=contenttypes')
         self.client = Client()
         self.favorite_path = reverse('MarvelUniverse:favorites')
@@ -21,6 +28,10 @@ class FavoriteViewTest(TestCase):
         self.favorite_series = FavoriteSeries.objects.create(user=self.user, series=self.series)
 
     def test_favorite_view(self):
+        """
+        Test the GET request to the favorite page.
+        Verifies that the response is 200 OK and the context contains the lists of favorite characters, comics, and series.
+        """
         self.client.force_login(self.user)
         response = self.client.get(self.favorite_path)
 
@@ -34,7 +45,14 @@ class FavoriteViewTest(TestCase):
 
 
 class ToggleFavoriteTest(TestCase):
+    """
+    Test suite for the functionality to toggle an item (character, comic, or series) as a favorite.
+    """
     def setUp(self):
+        """
+        Set up for testing the toggle favorite functionality.
+        Creates a test user and instances for character, comic, and series.
+        """
         self.client = Client()
         self.user = User.objects.create_user(username='Oattest', password='oat123')
         self.character = Character.objects.create(name='Spider-Man')
@@ -44,13 +62,15 @@ class ToggleFavoriteTest(TestCase):
         self.client.force_login(self.user)
 
     def test_toggle_favorite_character(self):
+        """
+        Test the toggle functionality for a favorite character.
+        Verifies that the character can be added and removed from the user's favorites.
+        """
         toggle_url = reverse('MarvelUniverse:toggle_favorite', args=['character', self.character.id])
-
         response = self.client.post(toggle_url)
         self.assertEqual(response.status_code, 200)
         self.assertTrue(response.json()['is_favorite'])
         self.assertTrue(FavoriteCharacter.objects.filter(user=self.user, character=self.character).exists())
-
         response = self.client.post(toggle_url)
         self.assertEqual(response.status_code, 200)
         self.assertFalse(response.json()['is_favorite'])
