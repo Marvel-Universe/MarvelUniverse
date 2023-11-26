0a1ab8be11015c98ea6caa55b87b2b22aabf8004
Jiratchaya Thongsuthum <92805190+mjrchy@users.noreply.github.com>
2023-11-26 15:14:31 +0700

    Implement quiz views and leaderboard

--- MarvelUniverse/views/quiz.py
+++ MarvelUniverse/views/quiz.py
@@ -7,6 +7,7 @@ from random import sample, shuffle, choice
 from django.urls import reverse
 from ..models import CharacterQuestion, CharacterChoice, ComicChoice, ComicQuestion, SeriesQuestion, SeriesChoice
 from django.contrib import messages
+from ..models import UserData
 
 
 class SelectQuizView(View):
@@ -69,32 +70,42 @@ def character_quiz_view(request, question_pk):
     except CharacterQuestion.DoesNotExist:
         messages.error(request, "Question not found")
         return HttpResponseRedirect(reverse('MarvelUniverse:select-quiz')) 
+    except CharacterChoice.DoesNotExist:
+        messages.error(request, "Choice not found")
+        return HttpResponseRedirect(reverse('MarvelUniverse:character-quiz', args=(question_pk,)))
 
     question_counter = request.session.get('question_counter', 0)
+    scores_per_game = request.session.get('scores_per_game', 0)
 
     if request.method == 'POST':
         choice_id = request.POST.get('choice_id')
         choice = get_object_or_404(CharacterChoice, pk=choice_id)
 
         if choice.is_correct:
-            messages.success(request, 'Correct!')
+            scores_per_game += 50
+            messages.success(request, 'Correct! earn +10 points.')
         else:
-            messages.success(request, 'Incorrect!')
+            scores_per_game -= 10
+            messages.success(request, 'Incorrect, -5 points try again!')
 
         question_counter += 1
         request.session['question_counter'] = question_counter
+        request.session['scores_per_game'] = scores_per_game
 
         if question_counter >= 10:
             messages.info(request, 'This is the last question!')
             request.session['question_counter'] = 0
-            return HttpResponseRedirect(reverse('MarvelUniverse:quiz-results'))
+            request.session['scores_per_game'] = scores_per_game
+            return HttpResponseRedirect(reverse('MarvelUniverse:leaderboard'))
 
         random_pk = random_question(1, 188)
         return HttpResponseRedirect(reverse('MarvelUniverse:character-quiz', args=(random_pk,)))
 
     context = {
         'question': question,
-        'choices': choices_list
+        'choices': choices_list,
+        'question_number': question_counter + 1, 
+        'scores_per_game': scores_per_game
     }
     return render(request, 'MarvelUniverse/quiz/character-quiz.html', context)
 
@@ -111,29 +122,42 @@ def comic_quiz_view(request, question_pk):
     except ComicQuestion.DoesNotExist:
         messages.error(request, "Question not found")
         return HttpResponseRedirect(reverse('MarvelUniverse:select-quiz')) 
+    except ComicChoice.DoesNotExist:
+        messages.error(request, "Choice not found")
+        return HttpResponseRedirect(reverse('MarvelUniverse:comic-quiz', args=(question_pk,)))
 
     question_counter = request.session.get('question_counter', 0)
+    scores_per_game = request.session.get('scores_per_game', 0)
 
     if request.method == 'POST':
         choice_id = request.POST.get('choice_id')
         choice = get_object_or_404(ComicChoice, pk=choice_id)
 
-        messages.success(request, 'Correct!' if choice.is_correct else 'Incorrect!')
+        if choice.is_correct:
+            scores_per_game += 50
+            messages.success(request, 'Correct! earn +10 points.')
+        else:
+            scores_per_game -= 10
+            messages.success(request, 'Incorrect, -5 points try again!')
 
         question_counter += 1
         request.session['question_counter'] = question_counter
+        request.session['scores_per_game'] = scores_per_game
 
         if question_counter >= 10:
             messages.info(request, 'This is the last question!')
             request.session['question_counter'] = 0
-            return HttpResponseRedirect(reverse('MarvelUniverse:quiz-results'))
+            request.session['scores_per_game'] = scores_per_game
+            return HttpResponseRedirect(reverse('MarvelUniverse:leaderboard'))
 
-        random_pk = random_question(3, 102)
+        random_pk = random_question(2, 102)
         return HttpResponseRedirect(reverse('MarvelUniverse:comic-quiz', args=(random_pk,)))
 
     context = {
         'question': question,
-        'choices': choices_list
+        'choices': choices_list,
+        'question_number': question_counter + 1, 
+        'scores_per_game': scores_per_game
     }
     return render(request, 'MarvelUniverse/quiz/comic-quiz.html', context)
 
@@ -149,15 +173,24 @@ def series_quiz_view(request, question_pk):
         shuffle(choices_list)
     except SeriesQuestion.DoesNotExist:
         messages.error(request, "Question not found")
-        return HttpResponseRedirect(reverse('MarvelUniverse:select-quiz'))  
+        return HttpResponseRedirect(reverse('MarvelUniverse:select-quiz')) 
+    except SeriesChoice.DoesNotExist:
+        messages.error(request, "Choice not found")
+        return HttpResponseRedirect(reverse('MarvelUniverse:series-quiz', args=(question_pk,)))
 
     question_counter = request.session.get('question_counter', 0)
+    scores_per_game = request.session.get('scores_per_game', 0)
 
     if request.method == 'POST':
         choice_id = request.POST.get('choice_id')
-        choice = get_object_or_404(ComicChoice, pk=choice_id)
+        choice = get_object_or_404(SeriesChoice, pk=choice_id)
 
-        messages.success(request, 'Correct!' if choice.is_correct else 'Incorrect!')
+        if choice.is_correct:
+            scores_per_game += 50
+            messages.success(request, 'Correct! earn +10 points.')
+        else:
+            scores_per_game -= 10
+            messages.success(request, 'Incorrect, -5 points try again!')
 
         question_counter += 1
         request.session['question_counter'] = question_counter
@@ -165,7 +198,8 @@ def series_quiz_view(request, question_pk):
         if question_counter >= 10:
             messages.info(request, 'This is the last question!')
             request.session['question_counter'] = 0
-            return HttpResponseRedirect(reverse('MarvelUniverse:quiz-results'))
+            request.session['scores_per_game'] = scores_per_game
+            return HttpResponseRedirect(reverse('MarvelUniverse:leaderboard'))
 
         random_pk = random_question(1, 100)
         return HttpResponseRedirect(reverse('MarvelUniverse:series-quiz', args=(random_pk,)))
@@ -178,5 +212,20 @@ def series_quiz_view(request, question_pk):
 
 
 @login_required(login_url='login')
-def quiz_results(request):
-    return render(request, 'MarvelUniverse/quiz/quiz-results.html')
+def leaderboard(request):
+    this_user = request.user
+    this_user_data, created = UserData.objects.get_or_create(user=this_user)
+    users_data = UserData.objects.all()
+    if 'scores_per_game' in request.session:
+        scores = request.session['scores_per_game']
+        del request.session['scores_per_game']
+        this_user_data.scores += scores
+        this_user_data.save()
+    else:
+        scores = 0
+    context = {
+        'user_scores': this_user_data.scores,
+        'top3_users_data': users_data.order_by('-scores')[0:3],
+        'other_users_data': users_data.order_by('-scores')[3:]
+    }
+    return render(request, 'MarvelUniverse/quiz/leaderboard.html',  context)
