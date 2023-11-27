0263d895d0513a79e146e2c6aaa31f2940b91157
Sankrub <navathon.l@ku.th>
2023-11-27 15:01:01 +0700

    improve function for search real time

--- MarvelUniverse/views/see_all.py
+++ MarvelUniverse/views/see_all.py
@@ -1,62 +1,61 @@
 from django.shortcuts import render
 from django.views import View
 from ..models import Character, Comic, Series
-from django.contrib import messages
+from django.http import JsonResponse
 
 
 class AllCharactersView(View):
     template_name = 'MarvelUniverse/see_all/all_characters.html'
 
     def get(self, request):
-        search_value = request.GET.get("get_search", "")
-        if search_value:
-            get_search = request.GET["get_search"]
-            characters = Character.objects.filter(name__icontains=get_search)[:]
-        else:
-            characters = Character.objects.all()
-        
+        characters = Character.objects.all()
         context = {
             'characters': characters,
             'character_count': characters.count(),
-            'get_search': search_value
         }
         return render(request, self.template_name, context)
+
+
+class CharacterSearchView(View):
+    def get(self, request):
+        search_query = request.GET.get("search", "")
+        characters = Character.objects.filter(name__icontains=search_query).values('name', 'image', 'id')
+        return JsonResponse(list(characters), safe=False)
 
 
 class AllComicsView(View):
     template_name = 'MarvelUniverse/see_all/all_comics.html'
 
     def get(self, request):
-        search_value = request.GET.get("get_search", "")
-        if search_value:
-            get_search = request.GET["get_search"]
-            comics = Comic.objects.filter(title__icontains=get_search)[:]
-        else:
-            comics = Comic.objects.all()
-
+        comics = Comic.objects.all()
         context = {
             'comics': comics,
             'comics_count': comics.count(),
-            'get_search': search_value
         }
         return render(request, self.template_name, context)
-    
+
+
+class ComicSearchView(View):
+    def get(self, request):
+        search_query = request.GET.get("search", "")
+        comic = Comic.objects.filter(title__icontains=search_query).values('title', 'image', 'id')
+        return JsonResponse(list(comic), safe=False)
+
 
 class AllSeriesView(View):
     template_name = 'MarvelUniverse/see_all/all_series.html'
 
     def get(self, request):
-        search_value = request.GET.get("get_search", "")
-        if search_value:
-            get_search = request.GET["get_search"]
-            series = Series.objects.filter(title__icontains=get_search)[:]
-        else:
-            series = Series.objects.all()
-
+        series = Series.objects.all()
         context = {
             'series': series,
             'series_count': series.count(),
-            'get_search': search_value
         }
         return render(request, self.template_name, context)
-    
+
+
+class SeriesSearchView(View):
+    def get(self, request):
+        search_query = request.GET.get("search", "")
+        series = Series.objects.filter(title__icontains=search_query).values("title", "image", "id")
+        return JsonResponse(list(series), safe=False)
