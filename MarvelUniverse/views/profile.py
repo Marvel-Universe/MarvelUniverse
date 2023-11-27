e778cef3014dc5b7f7738e8f0c271c6840d62f72
Warat Poovorakit <warat.po@ku.th>
2023-11-27 14:15:18 +0700

    implement require login for user profile page

--- MarvelUniverse/views/profile.py
+++ MarvelUniverse/views/profile.py
@@ -1,15 +1,15 @@
-# profile.py
 from django.http import JsonResponse
 from django.shortcuts import render
 from django.contrib.humanize.templatetags.humanize import naturaltime
 from django.views import View
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth.decorators import login_required  # Import login_required
 import json
 
 from MarvelUniverse.models import UserData
 
-
+@method_decorator(login_required(login_url='login'), name='dispatch')  # Apply login_required to all methods in the class
 class ProfileView(View):
     template_name = 'MarvelUniverse/profile.html'
 
@@ -30,9 +30,9 @@ class ProfileView(View):
 
         return render(request, self.template_name, context=context)
 
-
 @method_decorator(csrf_exempt, name='dispatch')
 class UpdateProfileImageView(View):
+    @method_decorator(login_required(login_url='login'))  # Apply login_required only to the post method
     def post(self, request, *args, **kwargs):
         data = json.loads(request.body)
         avatar_url = data.get('avatarUrl', '')
