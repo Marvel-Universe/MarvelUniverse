5a4246b31a7e0a93ef67fbf1369c5be594628928
Warat Poovorakit <warat.po@ku.th>
2023-11-28 16:37:37 +0700

    update route path to prevent all unknown path

--- mysite/urls.py
+++ mysite/urls.py
@@ -24,7 +24,6 @@ from django.views.generic import TemplateView
 
 
 urlpatterns = [
-    path('', RedirectView.as_view(url='marvel-universe/')),
     path('marvel-universe/', include('MarvelUniverse.urls')),
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
@@ -43,6 +42,6 @@ urlpatterns = [
          auth_views.PasswordResetCompleteView.as_view(template_name="registration/password_reset_complete.html"),
          name='password_reset_complete'),
     path('accounts/', include('allauth.urls')),
-    path('invalid-url/<str:path>', handle_invalid_url, name='invalid-url'),
-
+    path('', RedirectView.as_view(url='marvel-universe/')),
+    path('<str:invalid_path>/', handle_invalid_url, name='invalid-url'),
 ]
